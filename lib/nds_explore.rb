7c37151f348394fbf71a5553a8c27bc710fdd675
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-24 22:12:06 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,8 +6,11 @@ require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
+  puts nds
 end
 
 def print_first_directors_movie_titles
+  nds = directors_database
 
+  
 end
